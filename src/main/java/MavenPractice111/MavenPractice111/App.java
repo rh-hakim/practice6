66a83a42e01cec5adf7213e80ec032bb929910bb
unknown <rh.hakim2020@gmail.com>
2020-07-02 14:13:24 -0700

    second commit

---
+++ src/main/java/MavenPractice111/MavenPractice111/App.java
@@ -0,0 +1,13 @@
+package MavenPractice111.MavenPractice111;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
